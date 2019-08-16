ea24c3fc530103f4b14ce76ab798036fb6c73922
Nate Magera <nmagera07@gmail.com>
2019-08-16 12:36:58 -0400

    built out routes, working on migrations

--- projects/project-model.js
+++ projects/project-model.js
@@ -8,11 +8,34 @@ function getResources() {
     return db('resources')
 }
 
+function getTasks() {
+    return db('tasks')
+        .innerJoin('project_tasks_resources', 'tasks.id', 'project_tasks_resources.tasks_id')
+        .innerJoin('projects', 'project_tasks_resources.project_id', 'projects.id')
+        .select('projects.name', 'projects.project_description', 'tasks.task_description', 'tasks.completed')
+}
 
+function addProject(project) {
+    return db('projects')
+        .insert(project)
+}
 
+function addResource(resource) {
+    return db('resources')
+        .insert(resource)
+}
+
+function addTask(task) {
+    return db('tasks')
+        .insert(task)
+}
 
 module.exports = {
     getProjects,
     getResources,
-    
+    getTasks,
+    addProject,
+    addResource,
+    addTask,
+
 }
--- projects/project-router.js
+++ projects/project-router.js
@@ -22,5 +22,47 @@ router.get('/resources', async (req, res) => {
     }
 })
 
+router.get('/projects/tasks', async (req, res) => {
+    try {
+        const tasks = await Projects.getTasks()
+        res.json(tasks)
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to get list of tasks for projects'})
+    }
+})
+
+router.post('/projects', async ( req,res ) => {
+    const body = req.body
+
+    try {
+        const project = await Projects.addProject(body)
+        res.status(201).json(project)
+    } catch (error) {
+        res.status(500).json({ message: 'Could not add project.'})
+    }
+})
+
+router.post('/resources', async ( req,res ) => {
+    const body = req.body
+
+    try {
+        const resource = await Projects.addResource(body)
+        res.status(201).json(resource)
+    } catch (error) {
+        res.status(500).json({ message: 'Could not add resource.'})
+    }
+})
+
+router.post('/projects/tasks', async ( req,res ) => {
+    const body = req.body
+
+    try {
+        const task = await Projects.addTask(body)
+        res.status(201).json(task)
+    } catch (error) {
+        res.status(500).json({ message: 'Could not add task.'})
+    }
+})
+
 
 module.exports = router
